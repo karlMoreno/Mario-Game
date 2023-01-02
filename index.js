7b0a665a1bed55717faa7e3f4c53b9683cfdcd40
karlMoreno <karlcmoreno0811@gmail.com>
2023-01-01 22:40:33 -0800

    Player Creation

--- index.js
+++ index.js
@@ -1,5 +1,25 @@
 const canvas = document.querySelector('canvas')
 const c = canvas.getContext('2d')
 
+canvas.width = window.innerWidth // you dont need window
+canvas.height = window.innerHeight
 
-console.log(c)
+class Player {
+    constructor() {
+        this.position = {
+            x:100,
+            y:100
+        }
+        this.width = 30
+        this.height = 30
+    }
+
+    draw() {
+        c.fillStyle = 'red'
+        c.fillRect(this.position.x, this.position.y, this.width, this.height)
+    }
+}
+
+const player = new Player()
+
+player.draw()
