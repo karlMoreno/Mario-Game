21cd441fcedff3c9c8113d49e32c93b5215c84fc
karlMoreno <karlcmoreno0811@gmail.com>
2023-01-09 21:55:31 -0500

    Easy Win Scenario

--- index.js
+++ index.js
@@ -75,6 +75,9 @@ const keys = {
     }
 }
 
+
+let scrollOffset = 0
+
 function animate() {
     requestAnimationFrame(animate)
     c.clearRect(0,0,canvas.width,canvas.height)
@@ -92,15 +95,22 @@ function animate() {
         player.velocity.x = 0
 
         if(keys.right.pressed){
+            scrollOffset += 5
             platforms.forEach(platform => {
                 platform.position.x -= 5
             }) 
         } else if (keys.left.pressed) {
+            scrollOffset -= 5
             platforms.forEach(platform => {
                 platform.position.x += 5
             }) 
         }
     }
+    //WIN
+    if(scrollOffset === 2000){
+        console.log("You WIN WOOOOOOO")
+    }
+
     //platform collision
     platforms.forEach(platform => {
         if(
